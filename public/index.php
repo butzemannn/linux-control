d14810b4715cce4e957ca84e23355899bd152e06
Sven Thäle <sven.thaele@uni-ulm.de>
2021-06-23 23:48:33 +0200

    init folder structure

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>NAS Control</title> 
+        <?php include '../templates/buttons.php';?>
+    </head>
+    <body>
+
+    </body>
+</html>
